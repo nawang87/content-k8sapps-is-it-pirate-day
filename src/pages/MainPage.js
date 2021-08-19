6f4f5a51a4a5b2fd2abfb6e9b195b23c01996e56
Will Boyd <willb@linuxacademy.com>
2021-08-19 15:10:04 -0400

    make API configurable

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -10,7 +10,8 @@ import env from "@beam-australia/react-env";
 
 import ErrorSnackbar from '../components/ErrorSnackbar';
 
-console.log(`Environment message: ${env("PIRATE_MESSAGE")}`);
+const API = env("API") || 'http://localhost:3000';
+console.log(`API: ${API}`);
 
 const styles = theme => ({
   list: {
@@ -35,13 +36,39 @@ class MainPage extends Component {
     error: null,
   };
 
+  componentDidMount() {
+    this.getMessage();
+  }
+
+  async fetch(method, endpoint, body) {
+    try {
+      const response = await fetch(`${API}${endpoint}`, {
+        method,
+        body: body && JSON.stringify(body),
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+        },
+      });
+      return await response.text();
+    } catch (error) {
+      console.error(error);
+
+      this.setState({ error });
+    }
+  }
+
+  async getMessage() {
+    this.setState({ loading: false, message: (await this.fetch('get', '/')) || 'Failed to retrieve data' });
+  }
+
   render() {
     return (
       <Fragment>
         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
           <div>
             <img style={{width: '500px'}} src={pirateShip} alt="Pirate Ship"/>
-            <Typography variant="h1" align='center'>{env("PIRATE_MESSAGE")}</Typography>
+            <Typography variant="h1" align='center'>{this.state.message}</Typography>
           </div>
         </div>
         {this.state.error && (
